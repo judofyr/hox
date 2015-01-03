11e050b4f2f7fb5fcf90e95a441c8bbcabce8b31
Magnus Holm <judofyr@gmail.com>
2015-01-03 15:19:18 +0100

    Set response reason correctly

--- hox/server.nim
+++ hox/server.nim
@@ -2,8 +2,79 @@ import h2o
 import net
 import posix/posix
 import os
+import tables
 
 export h2o.TokenList, h2o.`==`, h2o.`$`
+
+const StatusCodeData = {
+  100: "Continue",
+  101: "Switching Protocols",
+  102: "Processing",
+  200: "OK",
+  201: "Created",
+  202: "Accepted",
+  203: "Non-Authoritative Information",
+  204: "No Content",
+  205: "Reset Content",
+  206: "Partial Content",
+  207: "Multi-Status",
+  208: "Already Reported",
+  226: "IM Used",
+  300: "Multiple Choices",
+  301: "Moved Permanently",
+  302: "Found",
+  303: "See Other",
+  304: "Not Modified",
+  305: "Use Proxy",
+  307: "Temporary Redirect",
+  308: "Permanent Redirect",
+  400: "Bad Request",
+  401: "Unauthorized",
+  402: "Payment Required",
+  403: "Forbidden",
+  404: "Not Found",
+  405: "Method Not Allowed",
+  406: "Not Acceptable",
+  407: "Proxy Authentication Required",
+  408: "Request Timeout",
+  409: "Conflict",
+  410: "Gone",
+  411: "Length Required",
+  412: "Precondition Failed",
+  413: "Payload Too Large",
+  414: "URI Too Long",
+  415: "Unsupported Media Type",
+  416: "Range Not Satisfiable",
+  417: "Expectation Failed",
+  422: "Unprocessable Entity",
+  423: "Locked",
+  424: "Failed Dependency",
+  426: "Upgrade Required",
+  428: "Precondition Required",
+  429: "Too Many Requests",
+  431: "Request Header Fields Too Large",
+  500: "Internal Server Error",
+  501: "Not Implemented",
+  502: "Bad Gateway",
+  503: "Service Unavailable",
+  504: "Gateway Timeout",
+  505: "HTTP Version Not Supported",
+  506: "Variant Also Negotiates",
+  507: "Insufficient Storage",
+  508: "Loop Detected",
+  510: "Not Extended",
+  511: "Network Authentication Required"
+}
+
+proc buildStatusCodeArray(): array[100..599, string] =
+  var table = StatusCodeData.newTable
+  for i in low(result)..high(result):
+    if table.hasKey(i):
+      result[i] = table[i]
+    else:
+      result[i] = table[(i div 100) * 100]
+
+const StatusCodes* = buildStatusCodeArray()
 
 type
   Server* = object
@@ -52,7 +123,7 @@ method call(self: ref BasicApp, req: BasicRequest, res: BasicResponse): bool =
 
 proc `status=`*(res: BasicResponse, code: int) =
   res.h2o_req.res.status = cint(code)
-  res.h2o_req.res.reason = "OK"
+  res.h2o_req.res.reason = StatusCodes[code]
 
 proc add_header*(res: BasicResponse, name: TokenList, value: string) =
   h2o_add_header(addr(res.h2o_req.pool), addr(res.h2o_req.res.headers), tokenPtr(name), value, value.len)
